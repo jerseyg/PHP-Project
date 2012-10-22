a14c677169ef6fb8cc83a809d7c80e5ef0b17bb4
Jersey Galapon <jerseyg@shaw.ca>
2012-10-22 09:45:38 -0500

    Init

---
+++ Gallery.php
@@ -0,0 +1,96 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title></title>
+	<!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<!-- Overriding CSS -->
+		<link rel="stylesheet" type="text/css" href="css/override.css">
+	<!-- End Overriding CSS -->
+	</head>
+	<body>
+		<!-- Body Container -->
+		<div class="container">
+
+			<!-- Title -->
+			<div class="row">
+				<div class="span12">
+					<h1>Captivating Photography</h1>
+					<!--<img src="http://fakeimg.pl/960x200/?text=Title&font=lobster" alt="image"> -->
+				</div>
+			</div>
+			<!-- End Title -->
+
+			<!-- Nav Bar -->
+			<div class="row">
+				<div class="span12">
+						<div class="navbar">
+						  <div class="navbar-inner">
+						    <a class="brand" href="#">Nav</a>
+						    <ul class="nav">
+						      <li><a href="/">Home</a></li>
+						      <li class="active"><a href="#">Gallery</a></li>
+						    </ul>
+						    <form class="navbar-search pull-right">
+ 							 <input type="text" class="search-query" placeholder="Search">
+							</form>
+						  </div>
+						</div>
+						<hr />
+				</div>
+		    </div>
+		    <!-- End Nav Bar -->
+
+		    <!-- Main Content/Sidebar -->
+		    <div class="row">
+
+		    	<!-- Content -->
+				<div class="span8">
+						<img src="http://fakeimg.pl/840x750/?text=Content&font=lobster" alt="image">
+				</div>
+				<!-- Sidebar -->
+					<div class="span4">
+						<ul class="nav nav-list">
+							<li class="nav-header"> Header</li>
+							<li class="active"><a href="#">Link</a></li>
+							<li class="active"><a href="#">Link</a></li>
+							<li class="active"><a href="#">Link</a></li>
+							<li class="active"><a href="#">Link</a></li>
+						</ul>
+
+						<div class="sidebar-content">
+							<h4>Announcements</h4>
+						<p><small>Bear claw apple pie donut tootsie roll marzipan wypas apple pie. Danish applicake cheesecake fruitcake. Carrot cake soufflé chocolate bar sweet roll wypas gummi bears. Pastry bear claw pie chocolate bar powder faworki ice cream caramels donut. Carrot cake cake jelly beans apple pie jelly-o tart jelly-o lemon drops gummi bears. Gummies halvah chocolate bonbon biscuit oat cake. Jelly muffin soufflé jujubes fruitcake chupa chups caramels dessert.</small></p>
+						<h4>Random Image</h4>
+						<img src="http://fakeimg.pl/240x120/?text=img&font=lobster" alt="image" class="random-img"> </div>
+				</div>
+
+		    </div>
+		    <!-- End Main Content -->
+
+		    <!-- Footer -->
+		    <div class="row">
+		    	<hr />
+				<div class="span3">
+						<img src="http://fakeimg.pl/840x100/?text=Copyright&font=lobster" alt="image">
+				</div>
+					<div class="span9">
+						<img src="http://fakeimg.pl/840x50/?text=footer&font=lobster" alt="image">
+				</div>
+		    </div>
+		    <!-- End Footer -->
+
+		</div>
+	    <!-- End Body Container -->
+	<!-- All Javascripts at end of code -->
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	</body>
+
+</html>
